61c64e497f25fd9207aba20c48ea8485976d6ce5
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2024-02-02 02:10:31 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,18 +1,16 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const faqsContainer = document.querySelector(".faqs-body");
+document.addEventListener("DOMContentLoaded", () => {
+  const faqs = document.querySelectorAll(".faq");
 
-  faqsContainer.addEventListener("click", function (event) {
-    const clickedElement = event.target;
-
-    if (clickedElement.classList.contains(".faq-icon-icon")) {
-      const faqItem = clickedElement.closet(".icons-icons");
-      const answer = faqItem.querySelector(".answer");
-      const plusIcon = clickedElement.querySelector(".plus-icon");
-      const minusIcon = clickedElement.querySelector(".minus-icon");
-
-      answer.classList.toggle("non-aactive");
-      plusIcon.classList.toggle("non-active");
-      minusIcon.classList.toggle("non-active");
-    }
+  faqs.forEach((faq) => {
+    faq.addEventListener("click", () => {
+      faq.classList.toggle("active");
+    });
   });
 });
+
+//margin: 0 auto;//
+//display: flex;
+//flex-direction: column;
+
+//margin-top: 20px;
+//margin-bottom: 12px;
