7efd84b424efe43314bc99aa5428e49b37dc5d46
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2024-02-05 21:58:16 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,24 +3,23 @@ document.addEventListener("DOMContentLoaded", () => {
 
   faqs.forEach((faq) => {
     faq.addEventListener("click", () => {
-      const isActive = faq.classList.toggle("active");
+      const activeFaq = document.querySelector(".faq.active");
+      if (activeFaq && activeFaq !== faq) {
+        activeFaq.classList.remove("active");
+      }
+
+      faq.classList.toggle("active");
 
       const plusIcon = question.querySelector(".plus-icon");
       const minusIcon = question.querySelector(".minus-icon");
 
-      if (isActive) {
-        plusIcon.classList.add("visible");
-        minusIcon.classList.remove("hidden");
+      if (faq.classList.contains("active")) {
+        plusIcon.style.display("none");
+        minusIcon.style.display("inline-block");
       } else {
-        plusIcon.classList.remove("visible");
-        minusIcon.classList.add("hidden");
+        plusIcon.style.display("inline-block");
+        minusIcon.style.display("none");
       }
     });
   });
 });
-
-//.question {
-    //display: grid;
-    //grid-template-rows: 1fr auto;
-    //align-items: flex-start;
-  //}
